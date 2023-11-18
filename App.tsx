04e2e34c1b46986f3529b7a7a63fac91dd6eeffa
Eduardo Pacheco <eduardo.p.carvalho6@aluno.senai.br>
2023-11-17 22:31:04 -0300

    Tela Welcome

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Login } from "./src/screens/Login";
 import { Loading } from "./src/screens/Loading";
+import { Welcome } from "./src/screens/Welcome";
 
 
 const App = () => {
     return (
 
-        <Login />
+        //<Login />
+        <Welcome />
     //    <Loading/>
     )
 };
---
+++ src/screens/Welcome/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import fonte from '../../assets/fonts/Squealer.otf'
+
+
+
+export const styles = StyleSheet.create({
+
+    background: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center'
+    },
+    image: {
+        width: '85%',
+        height: '50%',
+        marginTop: '40%'
+    
+    },
+    text: {
+        color: '#D1AE51',
+        fontSize: 30,
+        fontFamily: fonte
+    }
+
+})
