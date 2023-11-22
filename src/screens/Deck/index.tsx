2e1d7329b832ab70d2d21af41e489cd5c63497eb
Eduardo Pacheco <eduardo.p.carvalho6@aluno.senai.br>
2023-11-21 23:31:03 -0300

    [ADD] MODAL NO DECK, LIMITAÇÃO DECK, BLOQUEIO ADD

--- src/screens/Deck/index.tsx
+++ src/screens/Deck/index.tsx
@@ -1,10 +1,13 @@
-import { Text, View, Image, ImageBackground, FlatList } from "react-native";
+import { Text, View, Image, ImageBackground, FlatList, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 import carta from '../../assets/images/Dark.jpeg'
 import dragao from '../../assets/images/dragon.png'
 import { Loading } from "../Loading";
 import { useContext, useEffect, useState } from "react";
 import { LoginContext } from "../../contexts/LoginContext";
+import { CustomModal } from "../../components/ModalCarta";
+import { ButtonNav } from "../../components/ButtonNav";
+import { patchUsuarioDeck } from "../../services/ApiConta";
 
 
 
@@ -12,7 +15,16 @@ export const Deck = () => {
     
     const {usuario, infos} = useContext (LoginContext)
     const [deckUsuario, setDeckUsuario] = useState ([])
+    const [modalVisible, setModalVisible] = useState(false);
+    const [dataToPass, setDataToPass] = useState<any>(null);
 
+    useEffect(() => {
+        infos()
+    }, [])
+
+    useEffect(() => {
+    }, [usuario])
+   
     useEffect(()=>{
         infos()
         console.log(usuario.deck[0],"tudo errado");
@@ -34,10 +46,28 @@ export const Deck = () => {
     //     }
         
     const Card =({carta}:any) =>(
-        <View style={styles.cardContainer}>
+        <TouchableOpacity onPress={() => openModal({carta})} style={styles.cardContainer}>
             <Image source={{uri: carta.img}} style={styles.imgCard}/>
-        </View>
+        </TouchableOpacity>
     )
+
+    const closeModal = () => {
+        setDataToPass(null);
+        setModalVisible(false);
+    };
+
+    const openModal = (data: any) => {
+        setDataToPass(data.carta);
+        setModalVisible(true);
+    };
+
+    const removerDeck = async (cartaRemove: any) =>{
+        const deckDoUsuario = await usuario.deck;
+        const novoDeck = deckDoUsuario.filter(carta => carta.carta.id != cartaRemove.id);
+        await patchUsuarioDeck(usuario.id, novoDeck)
+        await infos()
+        closeModal()
+    }
 
     return (
         <ImageBackground source={dragao} style={styles.container}>
@@ -51,6 +81,14 @@ export const Deck = () => {
             numColumns={3}
             
             />
+              <CustomModal visible={modalVisible} onClose={closeModal}>
+                {dataToPass &&
+                    <View style={{ alignItems: "center", height: 460, width: "100%" }}>
+                        <Image source={{ uri: dataToPass.img }} style={[styles.imgCard, { height: 450 }]} />
+                        <ButtonNav style={{ backgroundColor: '#b88019' }} title="REMOVER DO DECK" openScreen={() => { removerDeck(dataToPass) }} />
+                    </View>}
+
+            </CustomModal>
         </ImageBackground>
     )
 }
